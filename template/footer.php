9d230a594adfcf79a9c1b2190e3aa256cb2d8287
Iván Portillo <ivan.portillo@kuehne-nagel.com>
2021-04-05 21:12:31 +0200

    Modificación versión jquery

--- template/footer.php
+++ template/footer.php
@@ -3,6 +3,13 @@
         <div class="small text-center text-muted">Copyright © 2020 - Start Bootstrap</div>
     </div>
 </footer>
+
+<!-- CDN Jquery -->
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+  
 <!-- Bootstrap core JS-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
