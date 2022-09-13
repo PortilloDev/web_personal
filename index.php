10ba8bf51547539148fce60d1b48111df67cb654
Iván Portillo <ivan.portillo@unir.net>
2022-09-13 07:54:01 +0200

    add tailwind

---
+++ index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+
+<head>
+  <title>Portfolio Iván Portillo</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+  <link rel="stylesheet" href="assets/css/main.css" />
+  <script src="https://cdn.tailwindcss.com"></script>
+  <noscript>
+    <link rel="stylesheet" href="assets/css/noscript.css" />
+  </noscript>
+</head>
+
+<body class="is-preload">
+
+  <!-- Sidebar -->
+  <section id="sidebar">
+    <div class="inner">
+      <nav>
+        <ul>
+          <li><a href="#intro">¿Quien soy?</a></li>
+          <li><a href="#one">Mis proyectos</a></li>
+          <li><a href="#two">¿Por que apostar por mi?</a></li>
+          <li><a href="#three">Contacto</a></li>
+        </ul>
+      </nav>
+    </div>
+  </section>
+
+  <!-- Wrapper -->
+  <div id="wrapper">
+
+    <!-- Intro -->
+    <section id="intro" class="wrapper style1 fullscreen fade-up">
+      <div class="inner">
+        <h1>Hola soy Iván, un <span style="color: #ffabe2;"> FullStack Developer en Laravel </span> y apasionado por la
+          programación. </h1>
+        <p>Me dedico a desarrollar en tecnologías web con Laravel y el stack TALL, PHP, JavaScript, Tailwind, etc.<br />
+          Te dejo mi <a href="https://github.com/PortilloDev" target="_blank">Mi GitHub</a> por si quieres ver mis
+          proyectos personales.</p>
+        <ul class="actions">
+          <li><a href="#one" class="button scrolly">Saber más 👇 </a></li>
+        </ul>
+      </div>
+    </section>
+
+    <div id="one" class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-5">
+                <!--Card 1-->
+                <div class="rounded overflow-hidden shadow-lg">
+                  <img class="w-full" src="images/pic03.jpg" alt="notas web">
+                  <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Blog notas web</div>
+                    <p class="text-white text-base">
+                        Blog desarrollado desde cero con Laravel 8 y el stack TALL. Blog que trata sobre programación y
+                        desarrollo web. La web tiene un panel de administración privado, que la convierte en totalmente
+                        administrable. Tanto para la creación de contenido en formato post, hasta
+                        las partes estáticas de la home y carga de imágenes. <a href="https://notasweb.me/blog" target="_blank" class="bg-white hover:bg-black text-gray-800 text-base font-semibold py-1 px-2 border border-gray-400 rounded-xl shadow">Learn more</a>
+                    </p>
+                  </div>
+                  <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#laravel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#php</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#cleancode</span>
+                  </div>
+                </div>
+                <!--Card 2-->
+                <div class="rounded overflow-hidden shadow-lg">
+                  <img class="w-full" src="images/pic01.jpg" alt="vue.exchange">
+                  <div class="px-6 py-4  mt-4">
+                    <div class="font-bold text-xl mb-2">Vue-exchange</div>
+                    <p class="text-white text-base">
+                        Proyecto desarrollado en Vue. Creación de un ranking de criptomonedas que muestra su valor en tiempo en
+                        real.    </p>
+                        <p class="text-white text-base">Proyecto realizado para un curso completado en la plataforma de Platzi. <a href="https://vue-exchange-pro.netlify.app/" target="_blank" class="bg-white hover:bg-black text-gray-800 text-base font-semibold py-1 px-2 border border-gray-400 rounded-xl shadow">Learn more</a></p>
+                  </div>
+                  <div class="px-6 pt-4 pb-2  mt-6">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#vue</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#javascript</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#netlify</span>
+                  </div>
+                </div>
+            
+                <!--Card 3-->
+                <div class="rounded overflow-hidden shadow-lg">
+                  <img class="w-full" src="images/pic02.jpg" alt="Landing Page">
+                  <div class="px-6 py-4">
+                    <div class="font-bold text-xl mb-2">Landing Page</div>
+                    <p class="text-white text-base">
+                        Landing Page desarrollada con el stack TALL de laravel. Integrada la notificación a usuario por correo y
+                        sincronización con la API de Mailchimp. <a href="https://github.com/PortilloDev/landing" target="_blank" class="bg-white hover:bg-black text-gray-800 text-base font-semibold py-1 px-2 border border-gray-400 rounded-xl shadow">Learn more</a>
+                    </p>
+                    
+                  </div>
+                  <div class="px-6 pt-4 pb-2">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#laravel</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#php</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#mailchimp</span>
+                  </div>
+                </div>
+                 <!--Card 4-->
+                 <div class="rounded overflow-hidden shadow-lg">
+                    <img class="w-full" src="images/react.jpg" alt="Portfolio React">
+                    <div class="px-6 py-4 mt-10">
+                      <div class="font-bold text-xl mb-2">Portfolio React</div>
+                      <p class="text-white text-base">
+                          Creación de un Porfolio para un perfil musical. Desarrollado en React y usa un Backend construido en Laravel por medio de Axios, para la integración 
+                          de servicio de correo. <a href="https://rafaelportillo.es/" target="_blank" class="bg-white hover:bg-black text-gray-800 text-base font-semibold py-1 px-2 border border-gray-400 rounded-xl shadow">Learn more</a>
+                      </p>
+                    </div>
+                  <div class="px-6 pt-4 pb-2 mt-10">
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#react</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#javascript</span>
+                    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#netlify</span>
+                  </div>
+                </div>
+            </div>
+    <!-- Two -->
+    <section id="two" class="wrapper style3 fade-up">
+      <div class="inner">
+        <h2>Qué puedo aportar</h2>
+        <p>Te aporto unos puntos para que conozcas un poco más sobre mi visión del desarrollo y de como trabajo.</p>
+        <div class="features">
+          <section>
+            <span class="icon solid major fa-code"></span>
+            <h3>Desarrollo</h3>
+            <p>Desarrollo con buenas prácticas y bajo arquitectura clean code y programación orientada a objetos.</p>
+          </section>
+          <section>
+            <span class="icon solid major fa-lock"></span>
+            <h3>Intereses</h3>
+            <p>Me gusta todo lo relacionado con el desarrollo web. Además tengo gran interes por la rama de la
+              computación cuantica y la inteligencia artificial.</p>
+          </section>
+          <section>
+            <span class="icon solid major fa-cog"></span>
+            <h3>Formación</h3>
+            <p>Técnico superior en informática y cursando actualmente el Grado de Ingeniería informática por la UNED.
+            </p>
+          </section>
+          <section>
+            <span class="icon solid major fa-desktop"></span>
+            <h3>Tecnologías</h3>
+            <p>Además de en Laravel, también he programado en Java, .Net y manejo de bases de datos relacionales.</p>
+          </section>
+          <section>
+            <span class="icon solid major fa-link"></span>
+            <h3>En equipo</h3>
+            <p>Me gusta contribuir y aportar a los equipos con los que trabajo, para poder crecer y mejorar, tanto el
+              equipo como el producto.</p>
+          </section>
+          <section>
+            <span class="icon major fa-gem"></span>
+            <h3>Experiencía</h3>
+            <p>Experiencia de 3 años programando, escalabilidad tanto a los proyectos como al grupo, gran capacidad de
+              aprendizaje y fácil adaptación a nuevos proyectos.</p>
+          </section>
+        </div>
+      </div>
+    </section>
+
+    <!-- Three -->
+    <section id="three" class="wrapper style1 fade-up">
+      <div class="inner">
+        <h2>Contacto</h2>
+        <p>Si deseas hablar conmigo puedes, contactarme por aquí </p>
+        <div class="split style1">
+          <section>
+            <h3 style="margin-bottom: 50px;">Social</h3>
+            <div class="row">
+              <div class="col-lg-2 mr-auto text-center">
+                <i class="fab fa-linkedin-in fa-2x mb-2 text-muted"></i>
+                <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
+                <a class="d-block" href="https://www.linkedin.com/in/iván-portillo-pérez-a9676878"
+                  target="_blank">Perfil en LinkedIn</a>
+              </div>
+              <div class="col-lg-2 mr-auto text-center">
+                <i class="fab fa-twitter fa-2x mb-2 text-muted"></i>
+                <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
+                <a class="d-block" href="https://twitter.com/portillo_dev" target="_blank">Perfil en Twitter</a>
+              </div>
+              <div class="col-lg-2 mr-auto text-center">
+                <i class="fab fa-instagram fa-2x mb-2 text-muted"></i>
+                <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
+                <a class="d-block" href="https://www.instagram.com/portillo_dev/" target="_blank">Perfil en
+                  Instagram</a>
+              </div>
+            </div>
+          </section>
+          <section>
+            <ul class="contact">
+              <li>
+                <h3>Dirección</h3>
+                <span>Fuenlabrada (Madrid)</span>
+              </li>
+              <li>
+                <h3>Email</h3>
+                <a href="#">ipp_1981@hotmail.com</a>
+              </li>
+              <li>
+                <h3>Teléfono</h3>
+                <span>682177179</span>
+              </li>
+
+            </ul>
+          </section>
+        </div>
+      </div>
+    </section>
+
+  </div>
+
+  <!-- Footer -->
+  <footer id="footer" class="wrapper style1-alt">
+    <div class="inner">
+      <ul class="menu">
+        <li>&copy; Portfolio realizado con una 🙂 </li>
+      </ul>
+    </div>
+  </footer>
+
+  <!-- Scripts -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery.scrollex.min.js"></script>
+  <script src="assets/js/jquery.scrolly.min.js"></script>
+  <script src="assets/js/browser.min.js"></script>
+  <script src="assets/js/breakpoints.min.js"></script>
+  <script src="assets/js/util.js"></script>
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
